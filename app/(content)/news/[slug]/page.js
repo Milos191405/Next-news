b99ed16c3b0db7d678fa7af5d5eddc2f301b6b6a
Milos191405 <milos.mirkovic7@gmail.com>
2025-03-19 14:31:34 +0100

    finishing routing

--- app/(content)/news/[slug]/page.js
+++ app/(content)/news/[slug]/page.js
@@ -1,5 +1,8 @@
+import {notFound} from "next/navigation";
+import Link from "next/link";
+
 import { DUMMY_NEWS } from "@/dummy-news";
-import { notFound } from "next/navigation";
+
 
 export default function NewsDetailPage ({params}) {
   const newsSlug = params.slug;
@@ -12,7 +15,9 @@ export default function NewsDetailPage ({params}) {
   return (
     <article className="news-article">
 <header>
+  <Link href={`/news/${newsItem.slug}/image`}>
   <img src={`/images/news/${newsItem.image}`} ald={newsItem.title}/>
+  </Link>
     <h1>{newsItem.title}</h1>
     <time dateTime={newsItem.date}>{newsItem.date}</time>
     </header>
